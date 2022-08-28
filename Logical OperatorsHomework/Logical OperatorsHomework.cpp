9daf327bc06069eb26655722f82a6a89bb524d59
MOHAMMAD <fardosi1994@gmail.com>
2022-08-28 16:26:22 +0300

    Logical OperatorsHomework Added

---
+++ Logical OperatorsHomework/Logical OperatorsHomework.cpp
@@ -0,0 +1,42 @@
+// Logical OperatorsHomework.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+using namespace std;
+#include <iostream>
+
+int main()
+{
+    cout << (12 >= 12) << endl;
+    cout << (12 > 7) << endl;
+    cout << (8 < 6) << endl;
+    cout << (8 == 8) << endl;
+    cout << (12 <= 12) << endl;
+    cout << (7 == 5) << endl;
+    cout << !(12 >= 12) << endl;
+    cout << !(12 < 7) << endl;
+    cout << !(8 < 6) << endl;
+    cout << !(8 == 8) << endl;
+    cout << !(12 <= 12) << endl;
+    cout << !(7 == 5) << endl;
+    cout << (1 && 1) << endl;
+    cout << (true && 0) << endl;
+    cout << (0 || 1) << endl;
+    cout << (0 || 0) << endl;
+    cout << !(0) << endl;
+    cout << !(1 || 0) << endl;
+    cout << (7 == 7 && 7 > 5) << endl;
+    cout << (7 == 7 && 7 < 5) << endl;
+    cout << (7 == 7 || 7 < 5) << endl;
+    cout << (7 == 7 || 7 > 5) << endl;
+    cout << !(7 == 7 && 7 > 5) << endl;
+    cout << (7 == 7 && !(7 < 5)) << endl;
+    cout << "*******************************************\n";
+
+    cout << ((5 > 6 && 7 == 7) || (1 || 0)) << endl;
+    cout << !((5 > 6 && 7 == 7) || (1 || 0)) << endl;
+    cout << ((5 > 6 && 7 == 7) || !(1 || 0)) << endl;
+    cout << !((5 > 6 || 7 == 7) && !(1 || 0)) << endl;
+    cout << ((5 > 6 && 7 <= 8) || (8 > 1 && 4 <= 3) && true) << endl;
+    cout << ((5 > 6 && !(7 <= 8)) && (8 > 1 || 4 <= 3) || true) << endl;
+
+}
+
