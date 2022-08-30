895d64fbb3ef327e4de5b65c411feaece19256db
MOHAMMAD <fardosi1994@gmail.com>
2022-08-30 16:33:31 +0300

    Functions Part 1 added

---
+++ Functions Part 1/Functions Part 1.cpp
@@ -0,0 +1,46 @@
+   
+#include <iostream>
+using namespace std;
+void DisplayMyCardInfo()
+{
+    cout << "**************************\n";
+    cout << "Name : Mouhamed-FARDOUSI\n";
+    cout << "Age : 28\n";
+    cout << "City : Istanbul\n";
+    cout << "Country : Turkiye\n";
+    cout << "**************************\n\n";
+}
+void PrintSquarStars()
+{
+    cout << "********\n";
+    cout << "********\n";
+    cout << "********\n";
+    cout << "********\n\n";
+}
+void PrintILoveProgramming()
+{
+    cout << "I Love Programming!\n";
+    cout << "I Promise To be The Best Developer ever!\n";
+    cout << "I Know it will take some time to practice,bu I will\n";
+    cout << "achieve my goal\n";
+    cout << "Best Regards,\n";
+    cout << "MOHAMAD FARDOUSI.\n\n";
+}
+void PrintH()
+{
+    cout << "*   *\n";
+    cout << "*   *\n";
+    cout << "*****\n";
+    cout << "*   *\n";
+    cout << "*   *\n";
+}
+int main()
+{
+    DisplayMyCardInfo();
+    PrintSquarStars();
+    PrintILoveProgramming();
+    PrintH();
+
+}
+
+
