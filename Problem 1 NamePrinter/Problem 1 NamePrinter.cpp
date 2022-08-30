3b98665959660b77ed74ce4e9e26682b008b7d13
MOHAMMAD <fardosi1994@gmail.com>
2022-08-30 19:00:09 +0300

    Problem 01 NamePrinter

---
+++ Problem 1 NamePrinter/Problem 1 NamePrinter.cpp
@@ -0,0 +1,14 @@
+// Problem 1 NamePrinter.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+void PrintmyName()
+{
+    std::cout << "Mohammed\n";
+}
+int main()
+{
+    PrintmyName();
+}
+
+
