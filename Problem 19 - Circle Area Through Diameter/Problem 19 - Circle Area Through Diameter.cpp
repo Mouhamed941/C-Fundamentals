e9c4d4cadf36ac56fed3585c5c26272368ecd594
MOHAMMAD <fardosi1994@gmail.com>
2022-08-30 19:48:28 +0300

    CircleAreaThroughDiameter function has been added

--- Problem 19 - Circle Area Through Diameter/Problem 19 - Circle Area Through Diameter.cpp
+++ Problem 19 - Circle Area Through Diameter/Problem 19 - Circle Area Through Diameter.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <cmath>;
  using namespace std;
+ float CircleAreaThroughDiameter(float Diameter)
+ {
+     const float Pi = 3.14;
+     float Area = (Pi * pow(Diameter, 2) / 4);
+     return Area;
+ }
 int main()
 {
     float Diameter;
-    const float Pi = 3.14;
+   
     cout << "Please Enter The Diameter of the Circle : \n";
     cin >> Diameter;
     cout << endl;
-    float Area = (Pi * pow(Diameter,2) / 4);
-    cout << "The Area of This Rectangele is : " << Area << endl;
+    
+    cout << "The Area of This Rectangele is : " << CircleAreaThroughDiameter( Diameter) << endl;
 }
 
 
