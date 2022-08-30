a1235534f317fd8c7e6024e85e745914e4725e87
MOHAMMAD <fardosi1994@gmail.com>
2022-08-30 19:44:47 +0300

    CircleArea function has been added

--- Problem 18 Circle Area/Problem 18 Circle Area.cpp
+++ Problem 18 Circle Area/Problem 18 Circle Area.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+double CircleArea(double Radius)
+{
+    const float Pi = 3.14;
+    double Area = Pi * pow(Radius, 2);
+    return Area;
+}
 int main()
 {
     cout << "Please Enter The Radius of Circle : \n";
     double Radius;
-    const float Pi = 3.14;
+    
     cin >> Radius;
 
-    double Area = Pi * pow(Radius, 2);
-    cout << Area << endl;
+    
+    cout << CircleArea(Radius) << endl;
 }
 
