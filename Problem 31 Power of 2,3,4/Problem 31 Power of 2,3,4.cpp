d1e85ebb2c37e9082998ff1ed794c70c62acf69b
MOHAMMAD <fardosi1994@gmail.com>
2022-08-31 11:46:04 +0300

    PowOf function added

--- Problem 31 Power of 2,3,4/Problem 31 Power of 2,3,4.cpp
+++ Problem 31 Power of 2,3,4/Problem 31 Power of 2,3,4.cpp
@@ -1,15 +1,20 @@
 #include <cmath>;
 #include <iostream>
 using namespace std;
+void PowOf(short Number)
+{
+
+    cout << pow(Number, 2) << endl;
+    cout << pow(Number, 3) << endl;
+    cout << pow(Number, 4) << endl;
+}
 int main()
 {
     cout << "Please Enter a Number : \n";
     short Number;
     cin >> Number;
     cout << endl;
+    PowOf(Number);
     
-    cout << pow(Number,2) << endl;
-    cout <<  pow(Number,3) << endl;
-    cout <<  pow(Number,4) << endl;
 }
 
