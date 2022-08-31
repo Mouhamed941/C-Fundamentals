f05932358c38dd48817509b6e99ef190b797457d
MOHAMMAD <fardosi1994@gmail.com>
2022-08-31 11:30:11 +0300

    Circle Area function added

--- Problem 20 Circle Area Inscribed in a Square/Problem 20 Circle Area Inscribed in a Square.cpp
+++ Problem 20 Circle Area Inscribed in a Square/Problem 20 Circle Area Inscribed in a Square.cpp
@@ -3,14 +3,20 @@
 using namespace std;
 #include <iostream>
 #include <cmath>;
+float CircleArea(float A)
+{
+    const float Pi = 3.14;
+    float Area = (Pi * pow(A, 2) / 4);
+    return Area;
+}
 int main()
 {
     float A;
-    const float Pi = 3.14;
+   
     cout << "Please Enter The Hieght of the Square : \n";
     cin >> A;
     cout << endl;
-    float Area = (Pi * pow(A, 2) / 4);
-    cout << "The Area of This Circle is : " << Area << endl;
+    
+    cout << "The Area of This Circle is : " << CircleArea(A) << endl;
 }
 
