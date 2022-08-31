fe54eb075d9e3412ee26374a37d3749e358f99ff
MOHAMMAD <fardosi1994@gmail.com>
2022-08-31 11:38:45 +0300

    TriangleArea function added

--- Problem 22 Circle Area Inscribed in an Isosceles Triangle/Problem 22 Circle Area Inscribed in an Isosceles Triangle.cpp
+++ Problem 22 Circle Area Inscribed in an Isosceles Triangle/Problem 22 Circle Area Inscribed in an Isosceles Triangle.cpp
@@ -2,19 +2,24 @@
 //
 using namespace std;
 #include <iostream>
-
+float TriangleArea(float TriangleHeight, float TriangleBase)
+{
+    const float Pi = 3.14159;
+    float Area = (Pi * (pow(TriangleBase, 2) / 4)) * ((2 * TriangleHeight - TriangleBase) / (2 * TriangleHeight + TriangleBase));
+    return Area;
+}
 int main()
 {
     float TriangleHeight, TriangleBase;
-    const float Pi = 3.14;
+    
     cout << "Please Enter The Heightof the Triangle: \n";
     cin >> TriangleHeight;
 
     cout << "Please Enter The Base  of the Triangle : \n";
     cin >> TriangleBase;
     cout << endl;
-    float Area = (Pi * (pow(TriangleBase,2) / 4)) * ((2 * TriangleHeight - TriangleBase) / (2 * TriangleHeight + TriangleBase));
-    cout << "The Area of This Circle is : " << Area << endl;
+    
+    cout << "The Area of This Circle is : " << TriangleArea(TriangleHeight,TriangleBase) << endl;
    
 }
 
