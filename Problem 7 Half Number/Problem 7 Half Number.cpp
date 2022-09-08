cf47bd3c31f7dae6685072db285c211c9500cc3f
MOHAMMAD <fardosi1994@gmail.com>
2022-09-08 15:06:24 +0300

    Problem 07 - Half Number

--- Problem 7 Half Number/Problem 7 Half Number.cpp
+++ Problem 7 Half Number/Problem 7 Half Number.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 using namespace std;
-int main()
+float ReadNumber()
 {
     cout << "Please Enter a Number :\n";
     float Number;
     cin >> Number;
-
-    cout << "Half of " << Number << " is " << Number / 2 << endl;
-
+    return Number;
+}
+float CalculateHalfOfNumber(float Num)
+{
+    return Num / 2;
+}
+void PrintResult(float Num)
+{
+    cout << "Half of The Number is : " << Num << endl;
+}
+int main()
+{
+    PrintResult(CalculateHalfOfNumber(ReadNumber()));
+    return 0;
 }
 
