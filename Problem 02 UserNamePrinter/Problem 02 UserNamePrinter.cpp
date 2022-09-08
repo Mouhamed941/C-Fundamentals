3d2fe4fc945cc767e9a285e1c849ee2ea800a668
MOHAMMAD <fardosi1994@gmail.com>
2022-09-08 10:13:26 +0300

    ReadName func created getline func used

--- Problem 02 UserNamePrinter/Problem 02 UserNamePrinter.cpp
+++ Problem 02 UserNamePrinter/Problem 02 UserNamePrinter.cpp
@@ -3,15 +3,19 @@
 using namespace std;
 #include <iostream>
 #include <string>
-string PrintUserName(string Name)
-{
-    return Name;
-}
-int main()
+void ReadName()
 {
     cout << "Please Enter Your Name : " << endl;
     string Name;
-    cin >> Name;
+    getline(cin, Name);
+}
+void PrintUserName(string Name)
+{
+    cout << "Your Name is : " << Name << endl;
+}
+int main()
+{
+    
     cout <<"Your Name is : " << PrintUserName(Name) << endl;
 }
 
