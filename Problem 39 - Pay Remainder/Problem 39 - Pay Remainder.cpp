b3a5eb7895844aa37c265b68a0e9eb8051bec376
MOHAMMAD <fardosi1994@gmail.com>
2022-09-16 21:16:18 +0300

    Problem 39 - Pay Remainder

--- Problem 39 - Pay Remainder/Problem 39 - Pay Remainder.cpp
+++ Problem 39 - Pay Remainder/Problem 39 - Pay Remainder.cpp
@@ -1,19 +1,32 @@
 using namespace std;
 #include <iostream>
+float ReadPositiveNumber(string Message)
+{
+    int Num;
+    do
+    {
+        cout << Message << endl;
+        cin >> Num;
+
+    } while (Num <= 0);
+
+    return Num;
+}
+int CalculateRemainder(short TotalBill, short CashPaid)
+{
+    int Remainder = CashPaid - TotalBill ;
+    return Remainder;
+}
 
 int main()
 {
-    cout << "Please Enter The Total Bill : \n";
-    short TotalBill;
-    cin >> TotalBill;
+    short TotalBill = ReadPositiveNumber("Please Enter the total bill : ");
+    short CashPaid= ReadPositiveNumber("Please Enter the Cash Paid : ");
 
-    cout << "Please Enter The Cash Paid : \n";
-    short CashPaid;
-    cin >> CashPaid;
-    cout << endl;
-
-    short Remainder = CashPaid - TotalBill;
-    cout << Remainder << endl;
+    cout << "Total Bill : " << TotalBill << endl;
+    cout << "Cash Paid : " << CashPaid << endl;
+    cout << "**********************\n";
+    cout << "Remainder : "<<CalculateRemainder(TotalBill, CashPaid);
 }
 
 
